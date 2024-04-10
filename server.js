f51dc526b866aa309845282476428170fe1a6e41
Testing Wizard <marcosmunozr15@gmail.com>
2024-04-09 23:11:44 -0600

    Added bonus features

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connection.connect((err) => {
     start();
 });
 
-// Function to display a formatted title
+// Function to display a stylized title
 function displayEmployeeTracker() {
     const employeeTracker = `
     
@@ -47,7 +47,7 @@ displayEmployeeTracker();
 
 
 
-// Function to Start the employee Tracker Application
+// Function to Start the app
 function start() {
     inquirer
         .prompt({
@@ -62,7 +62,14 @@ function start() {
                 "Add a role",
                 "Add an employee",
                 "Update an employee role",
-                "Exit the database",
+                //? BONUS OPTIONS
+                "Update employee managers",
+                "View employees by manager",
+                "View employees by department",
+                "Delete departments, roles, and employees",
+                "View the total utilized budget in each department",
+                //* EXIT
+                "Exit the database"
             ],
         })
         .then((answer) => {
@@ -85,12 +92,26 @@ function start() {
                 case "Add an employee":
                     addEmployee();
                     break;
-                case "Add a Manager":
-                    addManager();
-                    break;
                 case "Update an employee role":
                     updateEmployeeRole();
                     break;
+                //? BONUS
+                case "Update employee managers":
+                    addManager();
+                    break;
+                case "View employees by manager":
+                    viewEmployeesByManager();    
+                    break;
+                case "View employees by department":
+                    viewEmployeesByDepartment();
+                    break;
+                case "Delete departments, roles, and employees":
+                    deleteDepartmentsRolesEmployees();
+                    break;
+                case "View the total utilized budget in each department":
+                    viewTotalUtilizedBudgetOfDepartments();
+                    break;
+                //* EXIT
                 case "Exit the database":
                     connection.end();
                     console.log("See you next time!");
@@ -330,3 +351,275 @@ function updateEmployeeRole() {
         });
     });
 }
+
+// Function to add a Manager
+function addManager() {
+    const queryDepartments = "SELECT * FROM departments";
+    const queryEmployees = "SELECT * FROM employee";
+
+    connection.query(queryDepartments, (err, resDepartments) => {
+        if (err) throw err;
+        connection.query(queryEmployees, (err, resEmployees) => {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        type: "list",
+                        name: "department",
+                        message: "Select the department:",
+                        choices: resDepartments.map(
+                            (department) => department.department_name
+                        ),
+                    },
+                    {
+                        type: "list",
+                        name: "employee",
+                        message: "Select the employee to add a manager to:",
+                        choices: resEmployees.map(
+                            (employee) =>
+                                `${employee.first_name} ${employee.last_name}`
+                        ),
+                    },
+                    {
+                        type: "list",
+                        name: "manager",
+                        message: "Select the employee's manager:",
+                        choices: resEmployees.map(
+                            (employee) =>
+                                `${employee.first_name} ${employee.last_name}`
+                        ),
+                    },
+                ])
+                .then((answers) => {
+                    const department = resDepartments.find(
+                        (department) =>
+                            department.department_name === answers.department
+                    );
+                    const employee = resEmployees.find(
+                        (employee) =>
+                            `${employee.first_name} ${employee.last_name}` ===
+                            answers.employee
+                    );
+                    const manager = resEmployees.find(
+                        (employee) =>
+                            `${employee.first_name} ${employee.last_name}` ===
+                            answers.manager
+                    );
+                    const query =
+                        "UPDATE employee SET manager_id = ? WHERE id = ? AND role_id IN (SELECT id FROM roles WHERE department_id = ?)";
+                    connection.query(
+                        query,
+                        [manager.id, employee.id, department.id],
+                        (err, res) => {
+                            if (err) throw err;
+                            console.log(
+                                `Added manager ${manager.first_name} ${manager.last_name} to employee ${employee.first_name} ${employee.last_name} in department ${department.department_name}!`
+                            );
+                            // restart the application
+                            start();
+                        }
+                    );
+                });
+        });
+    });
+}
+
+// Function to View Employee By Manager
+function viewEmployeesByManager() {
+    const query = `
+      SELECT 
+        e.id, 
+        e.first_name, 
+        e.last_name, 
+        r.title, 
+        d.department_name, 
+        CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+      FROM 
+        employee e
+        INNER JOIN roles r ON e.role_id = r.id
+        INNER JOIN departments d ON r.department_id = d.id
+        LEFT JOIN employee m ON e.manager_id = m.id
+      ORDER BY 
+        manager_name, 
+        e.last_name, 
+        e.first_name
+    `;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        // group employees by manager
+        const employeesByManager = res.reduce((acc, cur) => {
+            const managerName = cur.manager_name;
+            if (acc[managerName]) {
+                acc[managerName].push(cur);
+            } else {
+                acc[managerName] = [cur];
+            }
+            return acc;
+        }, {});
+
+        // display employees by manager
+        console.log("Employees by manager:");
+        for (const managerName in employeesByManager) {
+            console.log(`\n${managerName}:`);
+            const employees = employeesByManager[managerName];
+            employees.forEach((employee) => {
+                console.log(
+                    `  ${employee.first_name} ${employee.last_name} | ${employee.title} | ${employee.department_name}`
+                );
+            });
+        }
+
+        // restart the application
+        start();
+    });
+}
+// Function to view Employees by Department
+function viewEmployeesByDepartment() {
+    const query =
+        "SELECT departments.department_name, employee.first_name, employee.last_name FROM employee INNER JOIN roles ON employee.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id ORDER BY departments.department_name ASC";
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("\nEmployees by department:");
+        console.table(res);
+        // restart the application
+        start();
+    });
+}
+
+// Function to Delete departments, roles, and employees
+function deleteDepartmentsRolesEmployees() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "deleteOption",
+            message: "What would you like to delete?",
+            choices: ["Departments", "Roles", "Employees"],
+        })
+        .then((answer) => {
+            switch (answer.deleteOption) {
+                case "Departments":
+                    deleteDepartment();
+                    break;
+                case "Roles":
+                    deleteRole();
+                    break;
+                case "Employees":
+                    deleteEmployee();
+                    break;
+            }
+        });
+}
+
+// Function to delete a department
+function deleteDepartment() {
+    const query = "SELECT * FROM departments";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        inquirer
+            .prompt({
+                type: "list",
+                name: "department",
+                message: "Select the department to delete:",
+                choices: res.map((department) => department.department_name),
+            })
+            .then((answer) => {
+                const department = res.find(
+                    (dept) => dept.department_name === answer.department
+                );
+                const query = "DELETE FROM departments WHERE id = ?";
+                connection.query(query, [department.id], (err, res) => {
+                    if (err) throw err;
+                    console.log(`Deleted department ${department.department_name} from the database!`);
+                    // Restart the application
+                    start();
+                });
+            });
+    });
+}
+
+// Function to delete a role
+function deleteRole() {
+    const query = "SELECT * FROM roles";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        inquirer
+            .prompt({
+                type: "list",
+                name: "role",
+                message: "Select the role to delete:",
+                choices: res.map((role) => role.title),
+            })
+            .then((answer) => {
+                const role = res.find((role) => role.title === answer.role);
+                const query = "DELETE FROM roles WHERE id = ?";
+                connection.query(query, [role.id], (err, res) => {
+                    if (err) throw err;
+                    console.log(`Deleted role ${role.title} from the database!`);
+                    // Restart the application
+                    start();
+                });
+            });
+    });
+}
+
+// Function to delete an employee
+function deleteEmployee() {
+    const query = "SELECT * FROM employee";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        inquirer
+            .prompt({
+                type: "list",
+                name: "employee",
+                message: "Select the employee to delete:",
+                choices: res.map(
+                    (employee) => `${employee.first_name} ${employee.last_name}`
+                ),
+            })
+            .then((answer) => {
+                const employee = res.find(
+                    (emp) =>
+                        `${emp.first_name} ${emp.last_name}` === answer.employee
+                );
+                const query = "DELETE FROM employee WHERE id = ?";
+                connection.query(query, [employee.id], (err, res) => {
+                    if (err) throw err;
+                    console.log(`Deleted employee ${answer.employee} from the database!`);
+                    // Restart the application
+                    start();
+                });
+            });
+    });
+}
+
+// Function to view the total utilized budget of a department
+function viewTotalUtilizedBudgetOfDepartments() {
+    const query = `
+    SELECT departments.department_name, SUM(roles.salary) AS utilized_budget
+    FROM employee
+    INNER JOIN roles ON employee.role_id = roles.id
+    INNER JOIN departments ON roles.department_id = departments.id
+    GROUP BY departments.id, departments.department_name
+    `;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        console.log("Total Utilized Budget of Each Department:");
+        res.forEach((row) => {
+            console.log(`${row.department_name}: $${row.utilized_budget}`);
+        });
+
+        // Restart the application
+        start();
+    });
+}
+
+
+
